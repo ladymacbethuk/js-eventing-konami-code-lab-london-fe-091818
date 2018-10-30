ea32121cbad2cc92226e9cb59d1690137943842a
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-30 18:39:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,11 +6,25 @@ function init() {
 
 }
 
-const input = document.querySelector('input')
-input.addEventListener('keydown', e => console.log(e.which))
-
-document.body.addEventListener('keydown', e => {
-  if (e.which === 'code') {
-    return alert('Hurray!')
+// Key codes for A, B, and C keys.
+const alphabet = [65, 66, 67];
+ 
+// Keep track of index outside of the event handler.
+let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
   }
-})
+}
